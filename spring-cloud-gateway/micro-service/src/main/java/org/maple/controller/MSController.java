d99ef821f22c7bbc5241af8686cdde2a92f18562
mapleins <mapleins@aliyun.com>
2021-01-21 15:51:42 +0800

    提交部分修改

--- spring-cloud-gateway/micro-service/src/main/java/org/maple/controller/MSController.java
+++ spring-cloud-gateway/micro-service/src/main/java/org/maple/controller/MSController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author mapleins
  */
-@RestController
+@RestController("testController")
 public class MSController {
 
     @GetMapping("/say")
     public String say(){
         return "hello 小子!";
+    }
+
+    @GetMapping("/ms/getOne")
+    public String getOne() {
+        return "getOne";
     }
 }
---
+++ spring-cloud-gateway/test-gateway/src/main/java/org/maple/GatewayApp.java
@@ -0,0 +1,14 @@
+package org.maple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mapleins
+ */
+@SpringBootApplication
+public class GatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
